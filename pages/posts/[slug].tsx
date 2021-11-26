5fe104a792ba35a3d190019a4567b9491cf5ce30
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2021-11-26 14:17:26 -0300

    feat: add styles to h1 tags

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { getMDXComponent } from "mdx-bundler/client";
 import { getAllPosts, getSinglePost } from "../../utils/mdx";
 import styles from "../../styles/Home.module.css";
+
+const Header1: React.FC = props => {
+  return <h1 {...props} className="largeTitle" />
+}
+
 const Post = ({ code, frontmatter }) => {
   const Component = React.useMemo(() => getMDXComponent(code), [code]);
 
   return (
     <div className={styles.container}>
-      <Component />
+      <Component components={{h1: Header1}}/>
     </div>
   );
 };
